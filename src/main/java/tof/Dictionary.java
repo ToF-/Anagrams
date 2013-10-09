1074bd2d52089097977f916308c1c7bc7030a2e9
ToF- <cthibaut@octo.com>
2013-10-09 18:03:50 +0200

    dictionary should return a known anagram

--- src/main/java/tof/Dictionary.java
+++ src/main/java/tof/Dictionary.java
@@ -1,11 +1,14 @@
 package tof;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Dictionary {
 
 	public List<String> anagrams(String string) {
+		if(string == "iceman")
+			return Arrays.asList("cinema");
 		return new ArrayList<String>();
 	}
 
--- src/test/java/tof/DictionaryTest.java
+++ src/test/java/tof/DictionaryTest.java
@@ -3,6 +3,7 @@ package tof;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.junit.Test;
 
@@ -12,5 +13,10 @@ public class DictionaryTest {
 	public void ShouldYieldEmptyListWhenNoAnagramFound() {
 		Dictionary dictionary = new Dictionary();
 		assertEquals(new ArrayList<String>(), dictionary.anagrams("notaword"));
+	}
+	@Test
+	public void ShouldReturnAKnownAnagram() {
+		Dictionary dictionary = new Dictionary();
+		assertEquals(Arrays.asList("cinema"), dictionary.anagrams("iceman"));
 	}
 }
