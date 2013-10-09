bf44aeccdbf14b9dd5617afe6b20b003f828d01b
ToF- <cthibaut@octo.com>
2013-10-09 18:24:27 +0200

    refactoring eliminating duplication but not the fake

--- src/main/java/tof/Dictionary.java
+++ src/main/java/tof/Dictionary.java
@@ -2,15 +2,18 @@ package tof;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class Dictionary {
 
-	public List<String> anagrams(String string) {
-		if(string == "iceman")
-			return Arrays.asList("cinema");
-		if(string == "file")
-			return Arrays.asList("life");
+	public List<String> anagrams(String word) {
+		HashMap<String,String> words = new HashMap<String,String>();
+		words.put("iceman","cinema");
+		words.put("file", "life");
+		if (words.containsKey(word)) {
+			return Arrays.asList(words.get(word));
+		}
 		return new ArrayList<String>();
 	}
 
